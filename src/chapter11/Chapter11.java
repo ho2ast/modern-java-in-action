a815b6ab767e8e73a86d943e45da8a179308f5fa
ho2ast <lucid7@hotmail.co.kr>
2023-10-28 02:21:29 +0900

    chapter11

--- src/chapter11/Chapter11.java
+++ src/chapter11/Chapter11.java
@@ -29,7 +29,28 @@ public class Chapter11 {
 
 //    null 값으로 Optional 만들기
     Optional<Car> car = Optional.ofNullable(new Car());
+
+//    map 으로 optional 값을 추출하고 변환하기
+    Optional<Insurance> optInsurance = Optional.ofNullable(new Insurance());
+    Optional<String> name = optInsurance.map(Insurance::getName);
+//    Optional의 map 메서드는 4장과 5장에서 살펴본 스트림의 map 메서드와 개념적으로 비슷하다.
+
+//    flatMap으로 Optional 객체 연결
+    Optional<Person> optPerson = Optional.of(new Person());
+//    Optional<String> name =
+//        optPerson.map(Person::getCar)
+//            .map(Car::getInsurance)
+//            .map(Insurance::getName);
+//    위 코드는 컴파일 되지 않는다. getCar는 Optional<Car>을 반환하므로 Optional<Optional<Car>> 형식의 객체다.
+//    flatMap을 사용하면 인수로 받은 함수를 적용해서 생성된 각각의 스트림에서 콘텐츠만 남긴다.
+//    즉, 함수를 적용해서 생성된 모든 스트림이 하나의 스트림으로 병합되어 평준화된다.
+//    이차원 Optional이 하나의 삼각형을 포함하는 하나의 Optional로 바뀐다.
+        Optional<String> name2 =
+        optPerson.flatMap(Person::getCar)
+            .flatMap(Car::getInsurance)
+            .map(Insurance::getName);
   }
+
   public static String getCarInsuranceName(Person person) {
 //    null 확인 코드 추가
 //    if (person != null) {
